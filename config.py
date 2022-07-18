d006c6781bcb8d5957919188e40136d49e1962b3
julieniut <92865222+julieniut@users.noreply.github.com>
2022-07-18 10:17:23 +0200

    Embed, DEL et CMOS

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 import interactions
 import os
 from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+import asyncio
+import datetime 
+from datetime import datetime
 
 load_dotenv()
 
@@ -34,6 +39,27 @@ async def cmd(ctx: interactions.CommandContext, sub_command: str, second_option:
     elif sub_command == "2000":
       await ctx.send(f"Link of the second")
 
+
+@bot.command()
+async def c(ctx):
+    embed = discord.Embed(
+    title="Commandes",
+    description= "\n{menus Affiche les configs"
+    "\n!del 'n' supprimer 'n' message "
+    "\n!cmos ",
+        url="https://github.com/MerlinConnected/DiscordBot",
+        timestamp=datetime.now(), 
+        color=0x1abc9c
+    )
+    msg = await ctx.send(embed=embed)
+
+@bot.command(name="del")
+async def delete(ctx, amount = 2):
+    await ctx.channel.purge(limit=amount)
+
+@bot.command()
+async def cmos(ctx):
+	await ctx.send("Par ici le clear CMOS --> https://www.youtube.com/watch?v=Fc0HIDKC1U0")
 
 
 bot.start()
