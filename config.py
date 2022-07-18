a02f2f654eff9ba49e10326f4a8f2fb56b55bd3a
julieniut <92865222+julieniut@users.noreply.github.com>
2022-07-18 11:20:53 +0200

    temp

--- config.py
+++ config.py
@@ -70,5 +70,21 @@ async def cmos(ctx):
         await each_message.delete()
     await ctx.send("Par ici le clear CMOS --> https://www.youtube.com/watch?v=Fc0HIDKC1U0")    
 
+@bot.command()
+async def temp(ctx):
+    messages = await ctx.channel.history(limit=1).flatten()
+    for each_message in messages:
+        await each_message.delete()
+    await ctx.send("Pour vérifier les températures de votre processeur :"
+"\n- Télécharger OCCT : https://www.ocbase.com/download"
+"\n- Fermer tous les programmes (Discord, Steam...)"
+"\n- Lancer OCCT "
+"\n- Aller dans l'onglet : 'Test' (colonne de gauche)"
+"\n- Sélectionner dans la partie planning des tests : 'CPU' "
+"\n- Définir la durée du test --> 15 minutes minimum (en cliquant sur le logo infini au dessus)"
+"\n- Tout laisser en auto et appuyer sur play "
+"\n- Une fois le test terminé, envoyez dans le salon discord un screen des températures de votre processeur qui se trouvent dans la partie 'Monitoring' (à droite)."
+"\nExemple ici: https://imgur.com/a/5oWKVfp")
+
 
 bot.start()
